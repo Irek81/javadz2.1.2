660310ff10210c224d26246598ee4f10555ad166
Irek81 <irekisanbirdin@gmail.com>
2021-07-19 12:59:54 +0500

    main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int amount1 = 200;
-        int plus = 600;
-
+        int amount = 200;
+        int plus = 1_001;
         int bonus;
-
-        int amount2;
 
         if (plus > 1_000) {
             bonus = plus / 100;
             System.out.println(bonus);
-            amount2 = amount1 + plus + bonus;
-            System.out.println(amount2);
         } else {
-            amount2 = amount1 + plus;
-            System.out.println(amount2);
+            bonus = 0;
         }
 
+        System.out.println(amount + plus + bonus);
     }
 }
